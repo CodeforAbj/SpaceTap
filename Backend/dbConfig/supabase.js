29cb192482c5ff7fd0192d94c03d24f0b9998ebc
Abhijeet Shrivastava <codeforAbj@gmail.com>
2024-10-26 15:02:13 +0530

    removed unneccessary console log

--- Backend/dbConfig/supabase.js
+++ Backend/dbConfig/supabase.js
@@ -3,9 +3,6 @@ dotenv.config();
 
 import { createClient } from "@supabase/supabase-js";
 
-console.log(process.env.SUPABASE_URL);
-console.log(process.env.SUPABASE_KEY);
-
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
